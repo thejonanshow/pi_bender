5b2fa64f528346dca5ed03f9783a18e3a786ae31
Jonan Scheffler <jonanscheffler@gmail.com>
2020-05-16 19:06:34 -0700

    Runner outputs for dry runs, does not execute.

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -7,9 +7,23 @@ RSpec.describe PiBender::Runner do
       expect { runner.run(command: test_command) }.to raise_error(PiBender::Runner::CallerError)
     end
 
-    it "executes the supplied command" do
+    it "executes exactly the supplied command" do
       expect(runner).to receive(:execute).with(test_command).once
       runner.run(command: test_command)
+    end
+
+    context "during a dry run" do
+      let(:runner) { PiBender::Runner.new(test_config, dry_run: true).disable_io }
+
+      it "outputs the supplied command" do
+        expect(runner).to receive(:output).with(/#{test_command}/).once
+        runner.run(command: test_command)
+      end
+
+      it "does NOT execute" do
+        expect(runner).not_to receive(:execute)
+        runner.run(command: test_command)
+      end
     end
   end
 end
